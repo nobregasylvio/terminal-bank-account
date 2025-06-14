064e1db902c5b47120001f9b72b3790e887c2129
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-06-13 22:27:05 -0300

    feat: coletando numero da conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,14 +1,24 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
   /**
-   * Desafio de sintaxe para praticar variáveis, entrada de dados e estrutura básica de um programa em Java.
+   * Desafio de sintaxe para praticar variáveis, entrada de dados e estrutura
+   * básica de um programa em Java.
    * 
    * Coletar informações do usuário e exibir mensagem final.
-   * int, Numero.
+   * int, Numero da conta.
    * String, Agencia.
    * String, Nome do Cliente.
    * double, Saldo.
    */
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Digite o número da Conta: ");
+    while (!scan.hasNextInt()) {
+      System.out.println("Por favor, insira um número válido!");
+      System.out.print("Digite o número da Conta: ");
+      scan.next();
     }
 }
