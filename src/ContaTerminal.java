970fe629fcc96af40b481c5522f95ce8188b6172
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-06-13 21:51:51 -0300

    doc: Javadoc com informações do projeto.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,13 @@
 public class ContaTerminal {
+  /**
+   * Desafio de sintaxe para praticar variáveis, entrada de dados e estrutura básica de um programa em Java.
+   * 
+   * Coletar informações do usuário e exibir mensagem final.
+   * int, Numero.
+   * String, Agencia.
+   * String, Nome do Cliente.
+   * double, Saldo.
+   */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
